0f585ec1cdd0531e6cf2cb22c323234e69347674
Valderhide <gpinsent@hotmail.com>
2022-01-14 18:00:42 -0330

    imported MenuItem not found in 'react-bootstrap'

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, { render } from 'react-dom';
 import './index.css';
+import {ButtonToolbar, MenuItem, DropdownButton} from 'react-bootstrap';
 
 class Box extends React.Component {
   selecetBox = () => {
@@ -49,6 +50,49 @@ class Grid extends React.Component {
         {rowsArr}
       </div>
     );
+  }
+}
+
+class Buttons extends React.Component{
+  handleSelect = (evt) =>{
+    this.props.gridSize(evt);
+  }
+  render(){
+    return(
+      <div className="center">
+        <ButtonToolbar>
+          <button className="btn btn-default" onClick={this.props.playButton}>
+          Play
+          </button>
+          <button className="btn btn-default" onClick={this.props.pauseButton}>
+         Pause
+          </button>
+          <button className="btn btn-default" onClick={this.props.clear}>
+          Clear
+          </button>
+          <button className="btn btn-default" onClick={this.props.slow}>
+          Slow
+          </button>
+          <button className="btn btn-default" onClick={this.props.fast}>
+          Fast
+          </button>
+          <button className="btn btn-default" onClick={this.props.seed}>
+          Seed
+          </button>
+          <DropdownButton
+          title="Grid Size"
+          id="size-menu"
+          onSelect={this.handleSelect}
+          >
+            <MenuItem eventKey="1">20x10</MenuItem>
+            <MenuItem eventKey="2">50x30</MenuItem>
+            <MenuItem eventKey="3">70x50</MenuItem>
+            </DropdownButton>
+        </ButtonToolbar>
+        </div>
+
+
+    )
   }
 }
 
@@ -133,6 +177,15 @@ componentDidMount(){
     return (
       <div>
         <h1> The Game of LIfe</h1>
+        <Buttons
+        playButton = {this.playButton}
+        pauseButton = {this.pauseButton}
+        slow={this.slow}
+        fast={this.fast}
+        clear={this.clear}
+        seed={this.seed}
+        gridSize={this.gridSize}
+        />
         <Grid
           gridFull={this.state.gridFull}
           rows={this.rows}
