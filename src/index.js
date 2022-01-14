79c6304507e9a83a6daf43d34f0779f548d92fa7
Valderhide <gpinsent@hotmail.com>
2022-01-14 14:43:50 -0330

    CSS implemented

--- src/index.js
+++ src/index.js
@@ -2,9 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class Grid extends React.Component{
+  render(){
+     return(
+       <div>
+         Grid
+         </div>
+     );
+  }
+}
+
 class Main extends React.Component {
   constructor() {
     super();
+
+
     this.state = {
       generation: 0,
     }
