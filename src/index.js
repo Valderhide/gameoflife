34695e6ad454c128feee4e88fb35f7277a924c4f
Valderhide <gpinsent@hotmail.com>
2022-01-14 16:55:09 -0330

    Grid populated with boxes

--- src/index.js
+++ src/index.js
@@ -2,62 +2,78 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Grid extends React.Component{
-  render(){
-const width = this.props.cols * 14;
-var rowsArr = []
+class Box extends React.Component {
+  selecetBox = () => {
+    this.props.selecetBox(this.props.row, this.props.col);
+  }
 
-var boxClass="";
-for (var i=0; i<this.props.rows; i++){
-  for (var j=0; j<this.props.cols; j++){
-  let boxId = i + "_" + j;
-
-  boxClass = this.props.gridFull[i][j] ? "box on":"box off";
-  rowsArr.push(
-    <Box
-    boxClass={boxClass}
-    key={boxId}
-    boxId={boxId}
-    row={i}
-    col={j}
-    selecetBox={this.props.selecetBox}
-    />
-  )
+  render() {
+    return (
+      <div
+        className={this.props.boxClass}
+        id={this.props.id}
+        onClick={this.selecetBox}
+      />
+    );
   }
 }
-     
+
+class Grid extends React.Component {
+  render() {
+    const width = (this.props.cols * 16) +1;
+    var rowsArr = []
+
+    var boxClass = "";
+    for (var i = 0; i < this.props.rows; i++) {
+      for (var j = 0; j < this.props.cols; j++) {
+        let boxId = i + "_" + j;
+
+        boxClass = this.props.gridFull[i][j] ? "box on" : "box off";
+        rowsArr.push(
+          <Box
+            boxClass={boxClass}
+            key={boxId}
+            boxId={boxId}
+            row={i}
+            col={j}
+            selecetBox={this.props.selecetBox}
+          />
+        )
+      }
+    }
 
 
-return(
-       <div className="grid" style={{width: width}}>
-         {{rowsArr}}
-         </div>
-     );
+
+    return (
+      <div className="grid" style={{ width: width }}>
+        {rowsArr}
+      </div>
+    );
   }
 }
 
 class Main extends React.Component {
   constructor() {
     super();
-this.speed = 100;
-this.rows = 30; 
-this.cols = 50
+    this.speed = 100;
+    this.rows = 30;
+    this.cols = 50
 
     this.state = {
       generation: 0,
       gridFull: Array(this.rows).fill().map(() => Array(this.cols).fill(false))
     }
   }
-  
+
   render() {
     return (
       <div>
         <h1> The Game of LIfe</h1>
         <Grid
-        gridFull={this.state.gridFull}
-        rows={this.rows}
-        cols={this.cols}
-        selecetBox = {this.selecetBox}
+          gridFull={this.state.gridFull}
+          rows={this.rows}
+          cols={this.cols}
+          selecetBox={this.selecetBox}
         />
         <h2> Generation: {this.state.generation}</h2>
       </div>
