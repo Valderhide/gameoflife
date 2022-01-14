6a2a53e94d59412342671b6ebbe3c9dbcb85e79a
Valderhide <gpinsent@hotmail.com>
2022-01-14 14:02:12 -0330

    Removed un-needed and created Main Component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class Main extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      generation: 0;
+    }
+  }
+  render(){
+    return(
+      <div>
+      <h1> The Game of LIfe</h1>
+      <Grid
+      />
+      <h2> Generation: {this.state.generation}</h2>
+      </div>
+    );
+  }
+}
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Main/>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +32,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
